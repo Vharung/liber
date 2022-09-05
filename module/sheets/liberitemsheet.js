d4cbda97d3afaf64094027c579c8c9b759441cef
Vharung <contact@alexandre-crochet.fr>
2022-09-05 18:05:10 +0200

    @extends

--- module/sheets/liberitemsheet.js
+++ module/sheets/liberitemsheet.js
@@ -1,4 +1,8 @@
-export class LiberItemSheet extends ItemSheet{
+/**
+ * Extend the base Actor document by defining a custom roll data structure which is ideal for the Simple system.
+ * @extends {Items}
+ */
+ export class LiberItemSheet extends ItemSheet{
     get template(){
         console.log(`Liber | Récupération du fichier html ${this.item.type}-sheet.`);
         return `systems/liber/templates/sheets/${this.item.type}-sheet.html`;
