66fc36fbd410ac0c6eab56f272e500d885e91766
Vharung <93044724+Vharung@users.noreply.github.com>
2025-04-17 18:28:11 +0200

    update

--- module/sheet/actor.js
+++ module/sheet/actor.js
@@ -1475,6 +1475,7 @@ export default class LiberCharacterSheet extends HandlebarsApplicationMixin(Acto
           actingCharName: actor.name,
           actingCharImg: actor.img,
           actingAbilName: visuel,
+          info:"Info",
           introText: label,
           succes:description
         };
